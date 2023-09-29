412f984d454d8d206101f130aa45233d293fe9a0
VITÓRIA LUIZA RODRIGUES <23143598@edu119.local>
2023-09-29 16:15:49 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Pokemon } from 'src/modules/pokemon';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pokedex_';
+  logo= "../assets/logo.png";
+  imgProfile: string = "../assets/photoPerfil.jpg";
+  links: string [] = ['Home', 'Pokedex', 'Video Game', 'Card Games', 'Eventos'];
+  pokemons: Pokemon[] = [
+    new Pokemon(1, 'Pikachu', ['Elétrico'], 'Esse é o Pikachu', '../assets/pikachu.jpg'),
+    new Pokemon(2, 'Bulbasaur', ['Grama', 'Veneno'], 'Este é o Bulbasaur', '../assets/bulbasaur.jpg'),
+    new Pokemon(1, 'Ivysaur', ['Fogo'], 'Este é o Ivysaur', '../assets/ivysaur.jpg'),
+    new Pokemon(2, 'Geodude', ['Pedra'], 'Este é o Geodude', '../assets/geodude.jpg'),
+  ];
+  selectdPokemon?: Pokemon;
+
+  showDetails(pokemon: Pokemon) {
+    this.selectdPokemon = pokemon;
+  }
+
+  closeModal() {
+    this.selectdPokemon = undefined;
+  }
+
+  filterValue: string = '';
+  filteredPokemon(): Pokemon[] {
+    if (!this.filterValue) {
+      return this.pokemons;
+    }
+    return this.pokemons.filter(pokemon => pokemon.name.toLowerCase().includes(this.filterValue.toLowerCase()))
+  }
+
 }
+
---
+++ src/modules/pokemon.ts
@@ -0,0 +1,15 @@
+export class Pokemon {
+    id: number;
+    name: string;
+    type: string[];
+    description: string; 
+    photo: string;
+
+      constructor(id: number, name: string, type: string[], description: string, photo: string) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.description = description;
+        this.photo = photo;
+      }
+}
